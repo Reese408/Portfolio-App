87b5ecd4dd7a3a3d42f6329328871c6ff5c0c710
Reese408 <reeseredman099@gmail.com>
2026-04-24 10:16:21 -0400

    feat: delete old pages (skills, experience, certs, projects list, contact form) and unused components

--- my-app/app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getResume } from '@/lib/content/loader';
-import ContactContent from '@/components/contact/ContactContent';
-
-export default function ContactPage() {
-  const resume = getResume();
-
-  return <ContactContent resume={resume} />;
-}
